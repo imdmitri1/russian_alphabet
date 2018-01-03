2a396b326be15adda9ab2d00a9457b323f0c802c
D <email.ds@protonmail.com>
2018-01-03 10:03:26 -0600

    add audio files with .wav extension for firefox browser, modify letter output collection, add favicon

--- index.js
+++ index.js
@@ -2,408 +2,406 @@ var keyData = {
     q: {
      letter: "Й",
       sound: new Howl({
-        src: ['sounds/ikr.mp3']
+        src: ['sounds/ikr.wav','sounds/ikr.mp3']
       })
     },
     w: {
      letter: "Ш",
       sound: new Howl({
-        src: ['sounds/sh.mp3']
+        src: ['sounds/sh.wav','sounds/sh.mp3']
       })
     },
     e: {
      letter: "E",
       sound: new Howl({
-        src: ['sounds/e.mp3']
+        src: ['sounds/e.wav','sounds/e.mp3']
       })
     },
     r: {
       letter: "Р",
       sound: new Howl({
-        src: ['sounds/r.mp3']
+        src: ['sounds/r.wav','sounds/r.mp3']
       })
     },
       t: {
       letter: "Т",
       sound: new Howl({
-        src: ['sounds/t.mp3']
+        src: ['sounds/t.wav','sounds/t.mp3']
       })
     },
     y: {
       letter: "У",
       sound: new Howl({
-        src: ['sounds/y.mp3']
+        src: ['sounds/y.wav','sounds/y.mp3']
       })
     },
     u: {
       letter: "Ю",
       sound: new Howl({
-        src: ['sounds/u.mp3']
+        src: ['sounds/u.wav','sounds/u.mp3']
       })
     },
     i: {
       letter: "И",
       sound: new Howl({
-        src: ['sounds/i.mp3']
+        src: ['sounds/i.wav','sounds/i.mp3']
       })
     },
     o: {
       letter: "О",
       sound: new Howl({
-        src: ['sounds/o.mp3']
+        src: ['sounds/o.wav','sounds/o.mp3']
       })
     },
     p: {
       letter: "П",
       sound: new Howl({
-        src: ['sounds/p.mp3']
+        src: ['sounds/p.wav','sounds/p.mp3']
       })
     },
     a: {
       letter: "А",
       sound: new Howl({
-        src: ['sounds/a.mp3']
+        src: ['sounds/a.wav','sounds/a.mp3']
       })
     },
     s: {
       letter: "С",
       sound: new Howl({
-        src: ['sounds/s.mp3']
+        src: ['sounds/s.wav','sounds/s.mp3']
       })
     },
       d: {
       letter: "Д",
       sound: new Howl({
-        src: ['sounds/d.mp3']
+        src: ['sounds/d.wav','sounds/d.mp3']
       })
     },
     f: {
       letter: "Ф",
       sound: new Howl({
-        src: ['sounds/f.mp3']
+        src: ['sounds/f.wav','sounds/f.mp3']
       })
     },
     g: {
       letter: "Г",
       sound: new Howl({
-        src: ['sounds/g.mp3']
+        src: ['sounds/g.wav','sounds/g.mp3']
       })
     },
     h: {
       letter: "Х",
       sound: new Howl({
-        src: ['sounds/h.mp3']
+        src: ['sounds/h.wav','sounds/h.mp3']
       })
     },
     j: {
       letter: "Ж",
       sound: new Howl({
-        src: ['sounds/j.mp3']
+        src: ['sounds/j.wav','sounds/j.mp3']
       })
     },
     k: {
       letter: "К",
       sound: new Howl({
-        src: ['sounds/k.mp3']
+        src: ['sounds/k.wav','sounds/k.mp3']
       })
     },
     l: {
       letter: "Л",
       sound: new Howl({
-        src: ['sounds/l.mp3']
+        src: ['sounds/l.wav','sounds/l.mp3']
       })
     },
     z: {
       letter: "З",
       sound: new Howl({
-        src: ['sounds/z.mp3']
+        src: ['sounds/z.wav','sounds/z.mp3']
       })
     },
     x: {
       letter: "Ч",
       sound: new Howl({
-        src: ['sounds/ch.mp3']
+        src: ['sounds/ch.wav','sounds/ch.mp3']
       })
     },
     c: {
       letter: "Ц",
       sound: new Howl({
-        src: ['sounds/c.mp3']
+        src: ['sounds/c.wav','sounds/c.mp3']
       })
     },
     v: {
       letter: "В",
       sound: new Howl({
-        src: ['sounds/v.mp3']
+        src: ['sounds/v.wav','sounds/v.mp3']
       })
     },
     b: {
       letter: "Б",
       sound: new Howl({
-        src: ['sounds/b.mp3']
+        src: ['sounds/b.wav','sounds/b.mp3']
       })
     },
     n: {
       letter: "Н",
       sound: new Howl({
-        src: ['sounds/n.mp3']
+        src: ['sounds/n.wav','sounds/n.mp3']
       })
     },
     m: {
       letter: "М",
       sound: new Howl({
-        src: ['sounds/m.mp3']
+        src: ['sounds/m.wav','sounds/m.mp3']
       })
     },
     "[": {
       letter: "Щ",
       sound: new Howl({
-        src: ['sounds/sch.mp3']
+        src: ['sounds/sch.wav','sounds/sch.mp3']
       })
     },
     "]": {
       letter: "Ъ",
       sound: new Howl({
-        src: ['sounds/tver.mp3']
+        src: ['sounds/tver.wav','sounds/tver.mp3']
       })
     },
     ";": {
       letter: "Ь",
       sound: new Howl({
-        src: ['sounds/meag.mp3']
+        src: ['sounds/meag.wav','sounds/meag.mp3']
       })
     },
     "'": {
       letter: "Э",
       sound: new Howl({
-        src: ['sounds/eie.mp3']
+        src: ['sounds/eie.wav','sounds/eie.mp3']
       })
     },
     ",": {
       letter: "Ы",
       sound: new Howl({
-        src: ['sounds/bii.mp3']
+        src: ['sounds/bii.wav','sounds/bii.mp3']
       })
     },
     ".": {
       letter: "Ё",
       sound: new Howl({
-        src: ['sounds/yo.mp3']
+        src: ['sounds/yo.wav','sounds/yo.mp3']
       })
     },
     "/": {
       letter: "Я",
       sound: new Howl({
-        src: ['sounds/ia.mp3']
+        src: ['sounds/ia.wav','sounds/ia.mp3']
       })
     },
     // russian keyboard:
     й: {
      letter: "Й",
       sound: new Howl({
-        src: ['sounds/ikr.mp3']
+        src: ['sounds/ikr.wav','sounds/ikr.mp3']
       })
     },
     ш: {
      letter: "Ш",
       sound: new Howl({
-        src: ['sounds/sh.mp3']
+        src: ['sounds/sh.wav','sounds/sh.mp3']
       })
     },
     е: {
      letter: "E",
       sound: new Howl({
-        src: ['sounds/e.mp3']
+        src: ['sounds/e.wav','sounds/e.mp3']
       })
     },
     р: {
       letter: "Р",
       sound: new Howl({
-        src: ['sounds/r.mp3']
+        src: ['sounds/r.wav','sounds/r.mp3']
       })
     },
       т: {
       letter: "Т",
       sound: new Howl({
-        src: ['sounds/t.mp3']
+        src: ['sounds/t.wav','sounds/t.mp3']
       })
     },
     у: {
       letter: "У",
       sound: new Howl({
-        src: ['sounds/y.mp3']
+        src: ['sounds/y.wav','sounds/y.mp3']
       })
     },
     ю: {
       letter: "Ю",
       sound: new Howl({
-        src: ['sounds/u.mp3']
+        src: ['sounds/u.wav','sounds/u.mp3']
       })
     },
     и: {
       letter: "И",
       sound: new Howl({
-        src: ['sounds/i.mp3']
+        src: ['sounds/i.wav','sounds/i.mp3']
       })
     },
     о: {
       letter: "О",
       sound: new Howl({
-        src: ['sounds/o.mp3']
+        src: ['sounds/o.wav','sounds/o.mp3']
       })
     },
     п: {
       letter: "П",
       sound: new Howl({
-        src: ['sounds/p.mp3']
+        src: ['sounds/p.wav','sounds/p.mp3']
       })
     },
     а: {
       letter: "А",
       sound: new Howl({
-        src: ['sounds/a.mp3']
+        src: ['sounds/a.wav','sounds/a.mp3']
       })
     },
     с: {
       letter: "С",
       sound: new Howl({
-        src: ['sounds/s.mp3']
+        src: ['sounds/s.wav','sounds/s.mp3']
       })
     },
       д: {
       letter: "Д",
       sound: new Howl({
-        src: ['sounds/d.mp3']
+        src: ['sounds/d.wav','sounds/d.mp3']
       })
     },
     ф: {
       letter: "Ф",
       sound: new Howl({
-        src: ['sounds/f.mp3']
+        src: ['sounds/f.wav','sounds/f.mp3']
       })
     },
     г: {
       letter: "Г",
       sound: new Howl({
-        src: ['sounds/g.mp3']
+        src: ['sounds/g.wav','sounds/g.mp3']
       })
     },
     х: {
       letter: "Х",
       sound: new Howl({
-        src: ['sounds/h.mp3']
+        src: ['sounds/h.wav','sounds/h.mp3']
       })
     },
     ж: {
       letter: "Ж",
       sound: new Howl({
-        src: ['sounds/j.mp3']
+        src: ['sounds/j.wav','sounds/j.mp3']
       })
     },
     к: {
       letter: "К",
       sound: new Howl({
-        src: ['sounds/k.mp3']
+        src: ['sounds/k.wav','sounds/k.mp3']
       })
     },
     л: {
       letter: "Л",
       sound: new Howl({
-        src: ['sounds/l.mp3']
+        src: ['sounds/l.wav','sounds/l.mp3']
       })
     },
     з: {
       letter: "З",
       sound: new Howl({
-        src: ['sounds/z.mp3']
+        src: ['sounds/z.wav','sounds/z.mp3']
       })
     },
     ч: {
       letter: "Ч",
       sound: new Howl({
-        src: ['sounds/ch.mp3']
+        src: ['sounds/ch.wav','sounds/ch.mp3']
       })
     },
     ц: {
       letter: "Ц",
       sound: new Howl({
-        src: ['sounds/c.mp3']
+        src: ['sounds/c.wav','sounds/c.mp3']
       })
     },
     в: {
       letter: "В",
       sound: new Howl({
-        src: ['sounds/v.mp3']
+        src: ['sounds/v.wav','sounds/v.mp3']
       })
     },
     б: {
       letter: "Б",
       sound: new Howl({
-        src: ['sounds/b.mp3']
+        src: ['sounds/b.wav','sounds/b.mp3']
       })
     },
     н: {
       letter: "Н",
       sound: new Howl({
-        src: ['sounds/n.mp3']
+        src: ['sounds/n.wav','sounds/n.mp3']
       })
     },
     м: {
       letter: "М",
       sound: new Howl({
-        src: ['sounds/m.mp3']
+        src: ['sounds/m.wav','sounds/m.mp3']
       })
     },
     "щ": {
       letter: "Щ",
       sound: new Howl({
-        src: ['sounds/sch.mp3']
+        src: ['sounds/sch.wav','sounds/sch.mp3']
       })
     },
     "ъ": {
       letter: "Ъ",
       sound: new Howl({
-        src: ['sounds/tver.mp3']
+        src: ['sounds/tver.wav','sounds/tver.mp3']
       })
     },
     "ь": {
       letter: "Ь",
       sound: new Howl({
-        src: ['sounds/meag.mp3']
+        src: ['sounds/meag.wav','sounds/meag.mp3']
       })
     },
     "э": {
       letter: "Э",
       sound: new Howl({
-        src: ['sounds/eie.mp3']
+        src: ['sounds/eie.wav','sounds/eie.mp3']
       })
     },
     "ы": {
       letter: "Ы",
       sound: new Howl({
-        src: ['sounds/bii.mp3']
+        src: ['sounds/bii.wav','sounds/bii.mp3']
       })
     },
     "ё": {
       letter: "Ё",
       sound: new Howl({
-        src: ['sounds/yo.mp3']
+        src: ['sounds/yo.wav','sounds/yo.mp3']
       })
     },
     "я": {
       letter: "Я",
       sound: new Howl({
-        src: ['sounds/ia.mp3']
+        src: ['sounds/ia.wav','sounds/ia.mp3']
       })
     }
   }
   var text = new PointText({
-    fontFamily: 'Ubuntu',
-    fontWeight: 'bold',
-    fontSize: 250,
-    shadowColor: new Color(0, 0, 0),
-    shadowBlur: 12,
-    shadowOffset: new Point(5, 5)
+  	content: 'Press any key to see and hear russian equivalent',
+    point: view.center,
+    justification: 'center',
+    fontSize: 25
   });
 
   function randomColor(){
@@ -423,6 +421,14 @@ var keyData = {
       var point = maxPoint * (0.7, 0.6) * randomPoint;
       text.position = point + (180, 180);
       text.content = keyData[event.key].letter;
+      text.style = {
+        fontSize: 250,
+        fontFamily: 'Ubuntu',
+        fontWeight: 'bold',
+        shadowColor: new Color(0, 0, 0),
+        shadowBlur: 12,
+        shadowOffset: new Point(5, 5)
+      }
       text.fillColor = randomColor();
       keyData[event.key].sound.play();
     }
